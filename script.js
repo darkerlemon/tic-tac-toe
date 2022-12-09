0c6c5f1bf280c043fa0b5d70daf12d2387c5f4c6
metehan <metehansazak@gmail.com>
2022-12-09 15:29:11 +0800

    add simple css and restart button

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ const gameBoard = (() => {
     const botMid = document.querySelector('.botMid');
     const botRight = document.querySelector('.botRight');
     const para = document.querySelector('.para');
+    const btn = document.querySelector('button');
     para.textContent = '';
     let player = 'p1';
     gameOver = false;
-   
+    btn.addEventListener('click', () => {
+        window.location.reload();
+    });
     document.querySelectorAll('.grid').forEach(item => {
 
         item.addEventListener('click', event => {
@@ -20,11 +23,11 @@ const gameBoard = (() => {
         player = 'p2';}
          else if (player == 'p2' && event.target.textContent == '' && gameOver == false) {event.target.textContent = 'o'
         player = 'p1'};
-    if ((topLeft.textContent == topMid.textContent && topMid.textContent == topRight.textContent && topLeft.textContent == 'x') || (midLeft.textContent == midMid.textContent && midMid.textContent == midRight.textContent && midMid.textContent == 'x') || (botLeft.textContent == botMid.textContent && botMid.textContent == botRight.textContent && botRight.textContent == 'x') || (topLeft.textContent == midLeft.textContent && midLeft.textContent == botLeft.textContent && topLeft.textContent == 'x') || (topMid.textContent == midMid.textContent && midMid.textContent == botMid.textContent && topMid.textContent == 'x') || (topRight.textContent == midRight.textContent && midRight.textContent == botRight.textContent && topRight.textContent == 'x') || (topLeft.textContent == midMid.textContent && midMid.textContent == botRight.textContent && topLeft.textContent == 'x') || (topRight.textContent == midMid.textContent && midMid.textContent == botLeft.textContent && botLeft.textContent == 'x')) {para.textContent = 'player 1 won!';
+    if ((topLeft.textContent == topMid.textContent && topMid.textContent == topRight.textContent && topLeft.textContent == 'x') || (midLeft.textContent == midMid.textContent && midMid.textContent == midRight.textContent && midMid.textContent == 'x') || (botLeft.textContent == botMid.textContent && botMid.textContent == botRight.textContent && botRight.textContent == 'x') || (topLeft.textContent == midLeft.textContent && midLeft.textContent == botLeft.textContent && topLeft.textContent == 'x') || (topMid.textContent == midMid.textContent && midMid.textContent == botMid.textContent && topMid.textContent == 'x') || (topRight.textContent == midRight.textContent && midRight.textContent == botRight.textContent && topRight.textContent == 'x') || (topLeft.textContent == midMid.textContent && midMid.textContent == botRight.textContent && topLeft.textContent == 'x') || (topRight.textContent == midMid.textContent && midMid.textContent == botLeft.textContent && botLeft.textContent == 'x')) {para.textContent = 'Player 1 won!';
     gameOver = true;}
-    else if ((topLeft.textContent == topMid.textContent && topMid.textContent == topRight.textContent && topLeft.textContent == 'o') || (midLeft.textContent == midMid.textContent && midMid.textContent == midRight.textContent && midMid.textContent == 'o') || (botLeft.textContent == botMid.textContent && botMid.textContent == botRight.textContent && botRight.textContent == 'o') || (topLeft.textContent == midLeft.textContent && midLeft.textContent == botLeft.textContent && topLeft.textContent == 'o') || (topMid.textContent == midMid.textContent && midMid.textContent == botMid.textContent && topMid.textContent == 'o') || (topRight.textContent == midRight.textContent && midRight.textContent == botRight.textContent && topRight.textContent == 'o') || (topLeft.textContent == midMid.textContent && midMid.textContent == botRight.textContent && topLeft.textContent == 'o') || (topRight.textContent == midMid.textContent && midMid.textContent == botLeft.textContent && botLeft.textContent == 'o')) {para.textContent = 'player 2 won!';
+    else if ((topLeft.textContent == topMid.textContent && topMid.textContent == topRight.textContent && topLeft.textContent == 'o') || (midLeft.textContent == midMid.textContent && midMid.textContent == midRight.textContent && midMid.textContent == 'o') || (botLeft.textContent == botMid.textContent && botMid.textContent == botRight.textContent && botRight.textContent == 'o') || (topLeft.textContent == midLeft.textContent && midLeft.textContent == botLeft.textContent && topLeft.textContent == 'o') || (topMid.textContent == midMid.textContent && midMid.textContent == botMid.textContent && topMid.textContent == 'o') || (topRight.textContent == midRight.textContent && midRight.textContent == botRight.textContent && topRight.textContent == 'o') || (topLeft.textContent == midMid.textContent && midMid.textContent == botRight.textContent && topLeft.textContent == 'o') || (topRight.textContent == midMid.textContent && midMid.textContent == botLeft.textContent && botLeft.textContent == 'o')) {para.textContent = 'Player 2 won!';
     gameOver = true;}
-    else if (topLeft.textContent.length == '1' && topMid.textContent.length == '1' && topRight.textContent.length == '1' && midLeft.textContent.length == '1' && midMid.textContent.length == '1' && midRight.textContent.length == '1' && botLeft.textContent.length == '1' && botMid.textContent.length == '1' && botRight.textContent.length == '1' && gameOver == false) {para.textContent = 'tie'}
+    else if (topLeft.textContent.length == '1' && topMid.textContent.length == '1' && topRight.textContent.length == '1' && midLeft.textContent.length == '1' && midMid.textContent.length == '1' && midRight.textContent.length == '1' && botLeft.textContent.length == '1' && botMid.textContent.length == '1' && botRight.textContent.length == '1' && gameOver == false) {para.textContent = 'It\'s a tie!'}
 })
 })
 })();
